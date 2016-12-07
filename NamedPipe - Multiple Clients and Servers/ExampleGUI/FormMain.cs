37a36f5e12110bd97e2043a3b3956bba74530f07
S Moh <saif.md@live.com>
2016-12-07 14:30:39 -0600

    Initial Commit

---
+++ NamedPipe - Multiple Clients and Servers/ExampleGUI/FormMain.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ExampleGUI
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClient_Click(object sender, EventArgs e)
+        {
+			//Hide();
+			//new FormClient().ShowDialog(this);
+			//Close();
+			new FormClient().Show();
+        }
+
+        private void buttonServer_Click(object sender, EventArgs e)
+        {
+			//Hide();
+			//new FormServer().ShowDialog(this);
+			//Close();
+			new FormServer().Show();
+        }
+    }
+}

3b97489437b48f840483c1806cd2799a65999396
S Moh <saif.md@live.com>
2016-12-07 14:36:24 -0600

    Resized Main

--- NamedPipe - Multiple Clients and Servers/ExampleGUI/FormMain.Designer.cs
+++ NamedPipe - Multiple Clients and Servers/ExampleGUI/FormMain.Designer.cs
@@ -28,42 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.buttonClient = new System.Windows.Forms.Button();
-            this.buttonServer = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // buttonClient
-            // 
-            this.buttonClient.Location = new System.Drawing.Point(13, 13);
-            this.buttonClient.Name = "buttonClient";
-            this.buttonClient.Size = new System.Drawing.Size(75, 23);
-            this.buttonClient.TabIndex = 0;
-            this.buttonClient.Text = "&Client";
-            this.buttonClient.UseVisualStyleBackColor = true;
-            this.buttonClient.Click += new System.EventHandler(this.buttonClient_Click);
-            // 
-            // buttonServer
-            // 
-            this.buttonServer.Location = new System.Drawing.Point(94, 13);
-            this.buttonServer.Name = "buttonServer";
-            this.buttonServer.Size = new System.Drawing.Size(75, 23);
-            this.buttonServer.TabIndex = 1;
-            this.buttonServer.Text = "&Server";
-            this.buttonServer.UseVisualStyleBackColor = true;
-            this.buttonServer.Click += new System.EventHandler(this.buttonServer_Click);
-            // 
-            // FormMain
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(185, 49);
-            this.Controls.Add(this.buttonServer);
-            this.Controls.Add(this.buttonClient);
-            this.MaximizeBox = false;
-            this.Name = "FormMain";
-            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-            this.Text = "Main";
-            this.ResumeLayout(false);
+			this.buttonClient = new System.Windows.Forms.Button();
+			this.buttonServer = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// buttonClient
+			// 
+			this.buttonClient.Location = new System.Drawing.Point(22, 30);
+			this.buttonClient.Name = "buttonClient";
+			this.buttonClient.Size = new System.Drawing.Size(75, 23);
+			this.buttonClient.TabIndex = 0;
+			this.buttonClient.Text = "&Client";
+			this.buttonClient.UseVisualStyleBackColor = true;
+			this.buttonClient.Click += new System.EventHandler(this.buttonClient_Click);
+			// 
+			// buttonServer
+			// 
+			this.buttonServer.Location = new System.Drawing.Point(133, 30);
+			this.buttonServer.Name = "buttonServer";
+			this.buttonServer.Size = new System.Drawing.Size(75, 23);
+			this.buttonServer.TabIndex = 1;
+			this.buttonServer.Text = "&Server";
+			this.buttonServer.UseVisualStyleBackColor = true;
+			this.buttonServer.Click += new System.EventHandler(this.buttonServer_Click);
+			// 
+			// FormMain
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(220, 88);
+			this.Controls.Add(this.buttonServer);
+			this.Controls.Add(this.buttonClient);
+			this.MaximizeBox = false;
+			this.Name = "FormMain";
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.Text = "Main";
+			this.ResumeLayout(false);
 
         }
 
